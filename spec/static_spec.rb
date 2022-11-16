29d6a83f2fa358aeab3ea968147dffcdae4b87b0
Ahmed Albaiti <ahmed.albaiti@smartpension.co.uk>
2022-11-16 15:09:51 +0400

    Configure spec

---
+++ spec/static_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'index route', type: :system do
+    it 'shows page content' do
+      visit '/'
+      expect(page).to have_content('Smartflix')
+    end
+end 
