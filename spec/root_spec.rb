3382a31eb846d10a1b609c1fbe29a6ffcc8f99bf
Ahmed Albaiti <ahmed.albaiti@smartpension.co.uk>
2022-11-17 18:55:13 +0400

    Fix rubocop offences

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe 'shows/index.html.haml', type: :view do
   it 'shows list of first 10 shows' do
     assign :shows, @shows = CSV.parse(File.read('lib/netflix_titles.csv'), headers: true).first(10)
     render
-    @shows.each { |show| expect(rendered).to have_content(show["title"]) }   
+    @shows.each { |show| expect(rendered).to have_content(show['title']) }
   end
 end
