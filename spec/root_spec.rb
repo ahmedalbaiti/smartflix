718f8f3f83ec496243d36b249eee79862c7b3aed
Ahmed Albaiti <ahmed.albaiti@smartpension.co.uk>
2022-11-17 18:05:53 +0400

    Remove unnecessary files, install rubocop and correct formatting issues

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 RSpec.describe 'index route', type: :system do
   it 'shows page content' do
     visit '/'
-    expect(page).to have_content("Dick Johnson Is Dead") 
-    expect(page).to have_content("Blood & Water") 
-    expect(page).to have_content("Ganglands") 
-    expect(page).to have_content("Jailbirds New Orleans") 
-    expect(page).to have_content("Midnight Mass")
-    expect(page).to have_content("Sankofa")
+    expect(page).to have_content('Dick Johnson Is Dead')
+    expect(page).to have_content('Blood & Water')
+    expect(page).to have_content('Ganglands')
+    expect(page).to have_content('Jailbirds New Orleans')
+    expect(page).to have_content('Midnight Mass')
+    expect(page).to have_content('Sankofa')
   end
 end
