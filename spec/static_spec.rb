3c4491813bec06a8bed0341a9a68f2943ca11a66
Ahmed Albaiti <ahmed.albaiti@smartpension.co.uk>
2022-11-17 09:54:15 +0400

    Fix indentation in spec file

--- spec/static_spec.rb
+++ spec/static_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'index route', type: :system do
-    it 'shows page content' do
-      visit '/'
-      expect(page).to have_content('Smartflix')
-    end
-end 
+  it 'shows page content' do
+    visit '/'
+    expect(page).to have_content('Smartflix')
+  end
+end
